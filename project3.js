aa947502b7a6805e3463b7b3852027c9986c1034
tushar <98016057+tushar453@users.noreply.github.com>
2022-12-26 23:07:09 +0530

    Add files via upload

---
+++ project3.js
@@ -0,0 +1,7 @@
+// var popup = document.getElementById("popup");
+// function open(){
+//     popup.classList.add("open-popup");
+// }
+// function close(){
+//     popup.classList.remove("open-popup");
+// }
